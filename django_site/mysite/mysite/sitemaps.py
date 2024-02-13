9bb6f2a46381f5cec388e069c1577132f118b754
Александр Шиманский <derbercompany@gmail.com>
2024-02-13 12:43:16 +0300

    commit

--- django_site/mysite/mysite/sitemaps.py
+++
@@ -1,7 +0,0 @@
-from blogapp.sitemap_blogapp import BlogSitemap
-from shopapp.sitemap_shopapp import ShopSitemap
-
-sitemaps = {
-    "blog": BlogSitemap,
-    "shop": ShopSitemap,
-}
--- django_site/mysite/shopapp/sitemap_shopapp.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.sitemaps import Sitemap
-
-class ShopSitemap(Sitemap):
-
-    changefreq = "never"
-    priority = 0.5
